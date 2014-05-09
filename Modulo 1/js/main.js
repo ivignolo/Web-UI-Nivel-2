d773cd1b5081d4782847ffdd2318891aeb7d9569
unknown <ignacio.vignolo@GLB-d0958.globant.com>
2014-05-09 14:47:41 -0300

    Uso de requireJS, handlebars, etc

--- Modulo 1/js/main.js
+++ Modulo 1/js/main.js
@@ -1,157 +1,117 @@
-/*
- * Contact Object
- */
-function Contact(id, name, surname, phone, email){
-	this.id = id;
-	this.name = name;
-	this.surname = surname;
-	this.phone = phone;
-	this.email = email;
-}
+require(['jquery','underscore','backbone','handlebars'],function($){
 
-/*
- * Array of contacts
- */
-var contacts = new Array();
-
-jQuery(document).ready(function(){
-	getContacts();
-})
-
-/*
- * Get input from the form, create a Contact objects with this data, and save it into localStorage, also add it to array
- */
-function getFormData(){
-	var name = jQuery('#name').val();
-	var surname = jQuery('#surname').val();
-	var email = jQuery('#email').val();
-	var phone = jQuery('#phone').val();
-	
-	var contact = new Contact(contacts.length, name, surname, phone, email);
-	contacts.push(contact);
-	addContactToList(contact);
-	saveContact(contact);
-}
-
-/*
- * Save contact in localStorage
- */
-function saveContact(contact){
-	if(localStorage){
-		var contactItem = JSON.stringify(contact);
-		localStorage.setItem(contact.id, contactItem);
-	}
-	else{
-		console.log("Local storage no encontrado")
-	}
-}
-
-/*
- * Add all contacts in array into the <ul> element
- */
-function addContactsToList() {
-    var ul = jQuery('#contactList');
-    var listFragment = document.createDocumentFragment();
-    for (var i = 0; i < contacts.length; i++) {
-        var contactitem = contacts[i];
-		if(contactitem != undefined){
-			var li = createNewLiItem(contactitem);
-			listFragment.appendChild(li);
+	var Contact = Backbone.Model.extend({
+		initialize : function(){
+			this.on('invalid',function(model,error){
+				alert(error);
+			});
+		},
+		defaults: function(){
+			return{
+				name : '',
+				surname : '',
+				phone : '',
+				email : ''
+			}
+		},
+		validate: function(attributes){
+			if(!attributes.name || !attributes.surname || !attributes.phone || !attributes.email){
+				return 'Por favor llenar todos los datos del formulario.';
+			}
 		}
-    }
-    ul.append(listFragment);
-}
+	});
 
-/*
- * Add Contact to the end of the <ul> element
- */
-function addContactToList(contactItem) {
-	var ul = jQuery('#contactList');
-    var li = createNewLiItem(contactItem);
-    ul.append(li);
-    jQuery('#contactForm')[0].reset();
-}
+	var ContactList = Backbone.Collection.extend({
+		model: Contact
+	});
 
-/*
- * Create new Contact <li> item for given contact object
- */
-function createNewLiItem(contactitem) {
-    var li = document.createElement("li");
-    var span = document.createElement("span");
-    span.innerHTML =
-		"<img src=\"images/contactImg.jpg\"/><br /><br />" +
-		"<label for=\"name\">Nombre:</label> <input id=\"name" + contactitem.id + "\" type=\"text\" value="+ contactitem.name+ "> <br /> " +
-        "<label for=\"surname\">Apellido:</label> <input id=\"surname" + contactitem.id + "\" type=\"text\" value="+ contactitem.surname+ "> <br /> " +
-		"<label for=\"email\">Email:</label> <input id=\"email" + contactitem.id + "\" type=\"text\" value="+ contactitem.email+ "> <br /> " +
-		"<label for=\"phone\">Telefono:</label> <input id=\"phone" + contactitem.id + "\" type=\"text\" value="+ contactitem.phone+ "> <br /> " +
-		"<div><button type=\"button\" onclick=\"deleteContact(" + contactitem.id + ");\">Borrar</button> <button type=\"button\" onclick=\"editContact(" + contactitem.id + ");\">Guardar cambios</button></div>";
+	var contactList = new ContactList();
 
-    li.appendChild(span);
-    return li;
-}
-
-/*
- * Get all contacts from localStorage and add it to the list of contacts in the page
- */
-function getContacts(){
-	if(localStorage){
-		for(var i = 0; i < localStorage.length; i++){
-			var key = localStorage.key(i);
-			var item = localStorage.getItem(key);
-			var contactItem = JSON.parse(item);
-			contacts[contactItem.id] = contactItem;
+	var ContactView = Backbone.View.extend({
+		model : new Contact(),
+		tagName : 'li',
+		events: {
+			'click .edit': 'edit',
+			'click .delete': 'delete',
+			'blur .name': 'close',
+			'keypress .name': 'onEnterUpdate'
+		},
+		initialize: function(){
+			/*UNDERSCORE*/
+			//this.template = _.template($('#contactTemplate').html());
+			/*UNDERSCORE*/
+			
+			/*HANDLEBARS*/
+			var source   = $("#contactTemplate").html();
+			this.template = Handlebars.compile(source);
+			/*HANDLEBARS*/
+		},
+		edit: function(ev) {
+			ev.preventDefault();
+			this.$('.name').attr('contenteditable', true).focus();
+		},
+		close: function(ev) {
+			var name = this.$('.name').text();
+			this.model.set('name', name);
+			this.$('.name').removeAttr('contenteditable');
+		},
+		onEnterUpdate: function(ev) {
+			var self = this;
+			if (ev.keyCode === 13) {
+				this.close();
+				self.$('.name').blur();
+			}
+		},
+		delete: function(ev) {
+			ev.preventDefault();
+			contactList.remove(this.model);
+		},
+		render: function(){
+			/*UNDERSCORE*/
+			//this.$el.html(this.template(this.model.toJSON()));
+			/*UNDERSCORE*/
+			
+			/*HANDLEBARS*/
+			var context = this.model.toJSON();
+			this.$el.html(this.template(context));
+			/*HANDLEBARS*/
+			
+			return this;
 		}
-		addContactsToList();
-	}else{
-		console.log("Local storage no encontrado")
-	}
-}
+	});
 
-/*
- * Delete contact
- */
-function deleteContact(contactId){
-	localStorage.removeItem(contactId);
-	delete contacts[contactId];
-	jQuery('#contactList').html("");
-	addContactsToList();
-	showDeleteSuccessMessage();
-}
+	var ContactsView = Backbone.View.extend({
+		model : contactList,
+		el: $('#contacts-container'),
+		initialize: function () {
+			this.model.on('add', this.render, this);
+			this.model.on('remove', this.render, this);
+		},
+		render: function(){
+			var self = this;
+			self.$el.html('');
+			this.model.each(function(contact, i){
+				self.$el.append((new ContactView({model: contact})).render().$el);
+			});
+			return this;
+		}
+	});
 
-/*
- * Edit contact
- */
-function editContact(contactId){
-	var item = localStorage.getItem(contactId);
-	var contactItem = JSON.parse(item);
+	$(document).ready(function(){
+		$('#contactForm').submit(function(){
+			var contact = new Contact();
+			contact.set({name: $('#name').val(),surname: $('#surname').val(), email: $('#email').val(), phone: $('#phone').val()});
+
+			if(contact.isValid()){
+				contactList.add(contact);
+			}	
+			//Imprimo lista en consola
+			console.log(JSON.stringify(contactList));
+			return false;
+		});
+		contactList.fetch({ url: "data/init.json"});
+		var appView = new ContactsView();
+	});
 	
-	//Update fields in object
-	contactItem.name = jQuery("#name" + contactId).val();
-	contactItem.surname = jQuery("#surname" + contactId).val();
-	contactItem.email = jQuery("#email" + contactId).val();
-	contactItem.phone = jQuery("#phone" + contactId).val();
-	
-	//Convert object to string and save in localStorage
-	var modifiedItem = JSON.stringify(contactItem);
-	localStorage.setItem(contactId,modifiedItem);
-	showSuccessMessage();
-}
-
-function showSuccessMessage(){
-	$('#msgPanelSuccess').show();
-	$('#msgPanelError').hide();
-	$('#msgPanelDeleteSuccess').hide();
-}
-
-function showErrorMessage(){
-	$('#msgPanelError').show();
-	$('#msgPanelSuccess').hide();
-	$('#msgPanelDeleteSuccess').hide();
-}
-
-function showDeleteSuccessMessage(){
-	$('#msgPanelDeleteSuccess').show();
-	$('#msgPanelSuccess').hide();
-	$('#msgPanelError').hide();
-}
+});
 
